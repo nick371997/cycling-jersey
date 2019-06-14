6a7750c7b781c4cd7ef84b83126053df6668aba7
nick371997 <31400910+nick371997@users.noreply.github.com>
2019-06-14 18:21:15 +0530

    Add files via upload

---
+++ adx/adx.ino
@@ -0,0 +1,126 @@
+ // analog input pin 5 -- voltage
+const int xpin = A0;                  // x-axis of the accelerometer1
+const int ypin = A1;                  // y-axis
+const int zpin = A2;                  // z-axis (only on 3-axis models) 
+
+const int xpin1 = A3;                  // x-axis of the accelerometer2
+const int ypin1 = A4;                  // y-axis
+const int zpin1 = A5;                  // z-axis (only on 3-axis models)
+
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(9600);
+
+  pinMode(8, OUTPUT); // left led
+  pinMode(9, OUTPUT); // front white led
+  pinMode(10,OUTPUT); // back red led
+  pinMode(11,OUTPUT); // right led
+}
+
+void loop() {
+
+  int   difference = map(difference,0, 1023, 10, 179) ; //nominl change in sensor value and detection
+  blinky();
+
+  int oldxpin = analogRead(xpin);
+  int oldypin = analogRead(ypin);
+  int oldzpin = analogRead(zpin);
+  int oldxpin1 = analogRead(xpin1);
+  int oldypin1 = analogRead(ypin1);
+  int oldzpin1 = analogRead(zpin1);
+  delay(100);
+  int currxpin = analogRead(xpin);
+  int currypin = analogRead(ypin);
+  int currzpin = analogRead(zpin);
+  int currxpin1 = analogRead(xpin1);
+  int currypin1 = analogRead(ypin1);
+  int currzpin1 = analogRead(zpin1);
+  // print the sensor values:
+  if((currxpin-oldxpin)>=difference)
+  Serial.print(analogRead(xpin));
+  else
+  Serial.print("null");
+  // print a tab between values:
+  Serial.print("\t");
+
+  if((currypin-oldypin)>=difference)
+  Serial.print(analogRead(ypin));
+  else
+  Serial.print("null");
+  // print a tab between values:
+  Serial.print("\t");
+
+  if((currzpin-oldzpin)>=difference)
+  Serial.print(analogRead(zpin));
+  else
+  Serial.print("null");
+
+  Serial.print("\t");
+
+  if((currxpin1-oldxpin1)>=difference)
+  Serial.print(analogRead(xpin1));
+  else
+  Serial.print("null");
+  // print a tab between values:
+  Serial.print("\t");
+
+  if((currypin1-oldypin1)>=difference)
+  Serial.print(analogRead(ypin1));
+  else
+  Serial.print("null");
+  // print a tab between values:
+  Serial.print("\t");
+
+  if((currzpin1-oldzpin1)>=difference)
+  Serial.print(analogRead(zpin1));
+  else
+  Serial.print("null");
+
+  if(currxpin-oldxpin>=difference || currypin-oldypin>=difference || currzpin-oldzpin>=difference)
+  {
+    Serial.println("flaggggggg");
+    digitalWrite(10,LOW);
+    digitalWrite(8,HIGH);
+    delay(100);
+    digitalWrite(8,LOW);
+    delay(100);
+  }
+  else 
+  digitalWrite(8,LOW);
+
+
+  
+
+   if(currxpin1-oldxpin1>=difference || currypin1-oldypin1>=difference || currzpin1-oldzpin1>=difference)
+  {
+    Serial.println("flaggggggg2");
+    digitalWrite(10,LOW);
+    digitalWrite(11,HIGH);
+    delay(100);
+    digitalWrite(11,LOW);
+    delay(100);
+  }
+  else 
+  digitalWrite(11,LOW);
+
+  
+    
+  Serial.println();
+  // delay before next reading:
+  delay(100);
+}
+
+
+
+void blinky()
+{
+  digitalWrite(9,HIGH);
+  digitalWrite(10,HIGH);
+  delay(100);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  delay(100);
+  
+}
+
---
+++ final_code/final_code.ino
@@ -0,0 +1,129 @@
+
+// avr-libc library includes
+#include <avr/io.h>
+#include <avr/interrupt.h>
+const int ypin = A2;                  // y-axis
+const int ypin1 = A3;
+volatile float a = 0;
+volatile float b = 0;
+volatile int flag;
+volatile int flag1;
+
+void setup() {
+  // initialize the serial communications:
+  Serial.begin(115200);
+
+
+  int k;
+  for (k = 3; k < 11; k++)
+  {
+    pinMode(k, OUTPUT);
+  }
+
+  // initialize Timer1
+  cli();          // disable global interrupts
+  TCCR1A = 0;     // set entire TCCR1A register to 0
+  TCCR1B = 0;     // same for TCCR1B
+
+  // set compare match register to desired timer count:
+  OCR1A = 15624 / 1000;
+  // turn on CTC mode:
+  TCCR1B |= (1 << WGM12);
+  // Set CS10 and CS12 bits for 1024 prescaler:
+  TCCR1B |= (1 << CS10);
+  TCCR1B |= (1 << CS12);
+  // enable timer compare interrupt:
+  TIMSK1 |= (1 << OCIE1A);
+  sei();          // enable global interrupts
+
+
+}
+
+void loop()
+{
+
+  float y = analogRead(ypin) - 350;
+  y = (y * 9.8) / 75;
+  float y1 = analogRead(ypin1) - 350;
+  y1 = (y1 * 9.8) / 75;
+
+  Serial.print(y);
+  Serial.print("\t");
+  Serial.print((y1));
+  Serial.print("\t");
+  Serial.print("\n");
+  a = y1;
+  b = y;
+
+
+  righton();
+  lefton();
+
+}
+
+
+
+void gesture()
+{
+  if (b < -2.5)
+    flag = 0;
+  else
+    flag = 1;
+}
+
+
+void gesture2()
+{
+  if (a < -2.5)
+    flag1 = 0;
+  else
+    flag1 = 1;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  gesture();
+  gesture2();
+}
+
+
+void lefton()
+{
+  if (flag == 0 && flag1 == 1)
+  {
+    int j = 3;
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+      for (j = 3; j < 8; j++)
+      {
+        digitalWrite(j, HIGH);
+        delay(50);
+        digitalWrite(j, LOW);
+        delay(50);
+      }
+    }
+  }
+}
+
+
+void righton()
+{
+  if (flag == 1 && flag1 == 0)
+  {
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+      int j = 8;
+      for (j = 8; j < 12; j++)
+      {
+        digitalWrite(j, HIGH);
+        delay(50);
+        digitalWrite(j, LOW);
+        delay(50);
+      }
+    }
+  }
+}
+
+
